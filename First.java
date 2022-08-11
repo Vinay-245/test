8e0d14aa5f2ec97032a44981ab5d600e5d0b0951
Vinay Kumar Mankala <vinay.m@comakeit.com>
2022-08-11 11:01:11 +0530

    Edited the Java program for the first time

--- First.java
+++ First.java
@@ -1,6 +1,8 @@
 import java.util.*;
 class First{
 public static void main(String args[]){
-    System.out.println("hello world");
+    Scanner sc= new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.println("Entered Number is " + a);
 }
 }
